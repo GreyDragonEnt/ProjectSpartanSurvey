6007b5a605161bd140dc3d633069513e6230a51f
GreyDragonEnt <james@greydragon.net>
2025-04-22 19:42:06 +0800

    Commit from Bolt to GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
@@ -12,6 +12,9 @@ export default defineConfig({
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
         },
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       },
     },
   },
